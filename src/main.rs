b4a6ec6359b6a913317c495fa8c353f36eb15320
MizukiTRL <natsumitrlwaifu@gmail.com>
2023-09-17 16:49:20 -0500

    Finished tictactoe

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 use std::fmt::Display;
 use std::io;
 use Tictac::Empty;
+use rand::Rng;
 
+#[derive(PartialEq)]
 enum Tictac {
     X,
     O,
@@ -43,7 +45,7 @@ fn read_input() -> (usize, usize) {
     cords
 }
 
-fn add_symbol(grid: &mut [[Tictac; 3]; 3], cords: (usize, usize)) -> bool{
+fn add_symbol(grid: &mut [[Tictac; 3]; 3], cords: (usize, usize), tic: Tictac) -> bool{
     match cords.0 {
         0..=2 => println!("{:?}", cords),
         _ => {
@@ -61,11 +63,32 @@ fn add_symbol(grid: &mut [[Tictac; 3]; 3], cords: (usize, usize)) -> bool{
     }
 
     match grid[cords.1][cords.0] {
-        Empty => grid[cords.1][cords.0] = Tictac::X,
+        Empty => grid[cords.1][cords.0] = tic,
         _ => return false,
     };
 
     true
+}
+
+fn check_win(grid: &[[Tictac; 3]; 3], tic: Tictac) -> bool{
+    //rows
+    for row in grid {
+        if(row[0] == tic && row[1] == tic && row[2] == tic){
+            return true;
+        }
+    }
+    //columns
+    for j in 0..3{
+        if grid[0][j] == tic && grid[1][j] == tic && grid[2][j] == tic{
+            return true;
+        }
+    }
+
+    if (grid[0][0] == tic && grid[1][1] == tic && grid[2][2] == tic) || (grid[0][2] == tic && grid[1][1] == tic && grid[2][0] == tic){
+        return true;
+    }
+
+    false
 }
 
 fn main() {
@@ -80,11 +103,28 @@ fn main() {
     loop {
         let cords = read_input();
 
-        match add_symbol(&mut grid, cords){
+        match add_symbol(&mut grid, cords, Tictac::X){
             true => (),
             false => continue,
         };
 
+        loop {
+            let cords: (usize, usize) = (rand::thread_rng().gen_range(0..3), rand::thread_rng().gen_range(0..3));
+            match add_symbol(&mut grid, cords, Tictac::O){
+                true => break,
+                false => continue,
+            };
+        }
+
         print_board(&mut grid);
+
+        if(check_win(&grid, Tictac::X)){
+            println!("you win");
+            break;
+        }
+        if(check_win(&grid, Tictac::O)){
+            println!("you lose");
+            break;
+        }
     }
 }
